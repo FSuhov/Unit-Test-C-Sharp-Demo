373babf5af9d1ad1e2e6e08f6a1da6ef726454bd
FSuhov <aksu@ukr.net>
2018-10-03 16:47:52 +0300

    Project created, StyleCop installed and initial requirements completed

---
+++ UnitTestTutorial/ProductionCode/Program.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace ProductionCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The program class contains an entry point for application.
+    /// </summary>
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
